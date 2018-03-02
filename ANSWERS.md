7bfd7ae5de61c03596152d6ce92a898ba7e3bd13
Tiger Fu <tigerfu@berkeley.edu>
2018-03-01 18:13:58 -0800

    h' '

--- ANSWERS.md
+++ ANSWERS.md
@@ -2,14 +2,20 @@
 
 1. What does the second 'nil' argument in the line 6 text_field_tag of teachers/new.html.erb represent?
 
+enables placeholders by setting the value inside the tag to nil
 
 2. Go to `localhost:3000/teachers` in your browser; why does this not work?
 
+no GET '/teachers' in routes.rb
 
 3. What type of request did your browser just perform?
 
+GET
 
 4. Go back to `localhost:3000/teachers/new`; submit the form again. What URL do you end up at?
 
+http://localhost:3000/teachers
 
 5. Why does `localhost:3000/teachers` work now?
+
+submitting the form posts route to '/teachers'
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,4 +1,4 @@
-class TeachersController < ApplicationController
+class StudentsController < ApplicationController
   def new
     @placeholder_course = '186'
   end
@@ -6,13 +6,13 @@ class TeachersController < ApplicationController
   def create
     # Hint: params??
 
-    @full_name = params[:full_name]
-    @course = params[:full_name]
-    @grade_level = params[:grade_level]
+    @length = params[:length]
+    @width = params[:width]
+    @height = params[:height]
 
     # This line is explicitly declaring that this controller method should render/serve the
-    # 'views/teachers/show.html.erb' view. Otherwise, the controller would try to look for a 
-    # 'views/teachers/create.html.erb' view file which we do not have. 
+    # 'views/Students/show.html.erb' view. Otherwise, the controller would try to look for a
+    # 'views/Students/create.html.erb' view file which we do not have.
     # By convention, create is associated with a POST/PUT method while show is associated with the GET HTTP method
     # so the result after a "create" (C in CRUD) is a show.
     render 'show'
